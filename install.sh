0d4ec51d3af42194fe270cfd4d59ac8d8b0bb72d
Joshua Magady <josh.magady@gmail.com>
2024-07-21 17:12:54 -0500

    feat: Add logging to installation script
    The update includes logging functions to the installation script. Echo statements were added to record the steps executed during the process - creation of destination directory, copying and overwriting of files. Existing copy command was also updated to force overwrite files.

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+log_file="$HOME/.config/fabric/install.log"
+
+logging() {
+    echo "$(date) : $1"
+}
+
 src_dir="./patterns/*"
 dest_dir="$HOME/.config/fabric/patterns"
 
 # Create the destination directory if not exists
+logging 'Creating destination directory if not exists.'
 mkdir -p "$dest_dir"
 
-# Copy the files
-cp -r $src_dir $dest_dir
+# Copy and overwrite the files
+logging 'Copying and overwriting the files.'
+cp -rf $src_dir $dest_dir
+logging 'Files copied successfully.'
