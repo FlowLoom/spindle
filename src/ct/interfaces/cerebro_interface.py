f065a9e4195e4cf016a3cbf50c2a6f932f534bea
Joshua Magady <josh.magady@gmail.com>
2024-07-25 13:54:12 -0500

    feat: Add ICerebro abstract interface
    Introduce an abstract interface ICerebro to the codebase. It includes methods for adding processors, parsers, and handlers as well as a run method, which will enable standardized interaction with cerebro components.

---
+++ src/ct/interfaces/cerebro_interface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Type, List
+from ct.interfaces import IProcessor, IParser, IHandler
+
+__All__ = ["ICerebro"]
+
+
+class ICerebro(ABC):
+    @abstractmethod
+    def add_processor(self, processor: Type[IProcessor]) -> None:
+        pass
+
+    @abstractmethod
+    def add_parser(self, parser: Type[IParser]) -> None:
+        pass
+
+    @abstractmethod
+    def add_handler(self, handler: Type[IHandler]) -> None:
+        pass
+
+    @abstractmethod
+    def run(self, *args, **kwargs) -> None:
+        pass
