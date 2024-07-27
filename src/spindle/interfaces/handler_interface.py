d776089e1aacdded0d2f730b62c7609992d7d842
Joshua Magady <josh.magady@gmail.com>
2024-07-27 15:47:16 -0500

    docs(interfaces): enhance docstring for IHandler class and handle method
    The commit updates the docstring for the `IHandler` class and its `handle` method, providing detailed descriptions of the class purpose, method parameters, return values, and potential exceptions.

--- src/spindle/interfaces/handler_interface.py
+++ src/spindle/interfaces/handler_interface.py
@@ -5,8 +5,25 @@ __All__ = ["IHandler"]
 
 
 class IHandler(ABC):
-    """Interface for handlers."""
+    """
+    Interface for handlers.
+
+    This abstract base class defines the interface for handlers that process parsed files.
+    """
+
     @abstractmethod
     def handle(self, parsed_files: Dict[str, List[str]]) -> None:
-        """Handles the parsed files' content."""
+        """
+        Handles the parsed files' content.
+
+        Args:
+            parsed_files (Dict[str, List[str]]): A dictionary containing parsed file contents.
+                The keys are file names, and the values are lists of strings representing the file content.
+
+        Returns:
+            None
+
+        Raises:
+            NotImplementedError: This method must be implemented by subclasses.
+        """
         pass
