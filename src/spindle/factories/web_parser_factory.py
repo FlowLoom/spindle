1139beb3b68608aab521af0a4d451928ff1e1b71
Joshua Magady <josh.magady@gmail.com>
2024-07-27 15:30:33 -0500

    docs(factories): add docstrings for methods and class
    This commit adds detailed docstrings to the WebParserFactory class and its methods, improving code documentation and clarity for future developers.

--- src/spindle/factories/web_parser_factory.py
+++ src/spindle/factories/web_parser_factory.py
@@ -7,11 +7,43 @@ __All__ = ['WebParserFactory']
 
 
 class WebParserFactory(AbstractParserFactory):
+    """
+    A concrete factory for creating web parsing components.
+
+    This factory implements the AbstractParserFactory interface to create
+    specific instances of parser, processor, and handler for web parsing.
+    """
     def _create_concrete_parser(self, *args, **kwargs) -> WebParser:
+        """
+        Create and return a WebParser instance.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            WebParser: An instance of WebParser with an associated processor.
+        """
         return WebParser(self.create_processor(), *args, **kwargs)
 
     def _create_concrete_processor(self) -> WebProcessor:
+        """
+        Create and return a WebProcessor instance.
+
+        Returns:
+            WebProcessor: An instance of WebProcessor.
+        """
         return WebProcessor()
 
     def _create_concrete_handler(self, *args, **kwargs) -> FileHandler:
+        """
+        Create and return a FileHandler instance.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            FileHandler: An instance of FileHandler.
+        """
         return FileHandler(*args, **kwargs)
