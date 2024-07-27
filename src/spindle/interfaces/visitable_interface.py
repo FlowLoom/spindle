0162d44ee0d2609349e88db9a29546006b130dd8
Joshua Magady <josh.magady@gmail.com>
2024-07-27 15:47:41 -0500

    docs(interfaces): add detailed docstrings to IVisitable class and accept method
    This commit enhances the documentation by adding comprehensive docstrings to the IVisitable class and its accept method, clarifying their roles and usage within the Visitor pattern.

--- src/spindle/interfaces/visitable_interface.py
+++ src/spindle/interfaces/visitable_interface.py
@@ -5,6 +5,26 @@ __All__ = ['IVisitable']
 
 
 class IVisitable(ABC):
+    """
+    An abstract base class representing a visitable object in the Visitor pattern.
+
+    This class defines the interface for objects that can be visited by a Visitor.
+    Classes that implement this interface agree to accept a Visitor object as an argument
+    to their accept method, allowing the Visitor to perform operations on the Visitable object.
+    """
+
     @abstractmethod
     def accept(self, visitor: IVisitor):
+        """
+        Accept a Visitor object to perform operations on this Visitable object.
+
+        This method should be implemented by concrete classes to allow a Visitor
+        to perform operations on the Visitable object.
+
+        Args:
+            visitor (IVisitor): The Visitor object that will perform operations on this Visitable.
+
+        Returns:
+            None
+        """
         pass
