7be243b415a1f49b2703a5e357a1e2cfb98369c7
Joshua Magady <josh.magady@gmail.com>
2024-07-27 15:47:25 -0500

    docs(interfaces): enhance docstrings for IParser class and parse method
    This commit improves the docstrings in the IParser class, providing more detailed explanations and clarifying the purpose and return value of the parse method.

--- src/spindle/interfaces/parser_interface.py
+++ src/spindle/interfaces/parser_interface.py
@@ -6,9 +6,22 @@ __All__ = ['IParser']
 
 
 class IParser(IVisitable):
-    """Interface for parsers."""
+    """
+    Interface for parsers.
+
+    This abstract base class defines the contract for parser implementations.
+    It inherits from IVisitable, suggesting that parser objects can be visited.
+    """
 
     @abstractmethod
     def parse(self) -> Dict[str, List[str]]:
-        """Parses files in the src directory."""
+        """
+        Parses content.
+
+        This abstract method must be implemented by concrete parser classes.
+
+        Returns:
+            Dict[str, List[str]]: A dictionary where keys are likely file names or identifiers,
+                                  and values are lists of strings representing parsed content.
+        """
         pass
