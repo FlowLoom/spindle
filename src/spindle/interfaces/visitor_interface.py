201365441cd8f4641a1657a86d188f33c63f92fb
Joshua Magady <josh.magady@gmail.com>
2024-07-27 15:47:53 -0500

    docs(interfaces): add class and method docstrings for IVisitor
    This commit enhances the `IVisitor` interface with detailed docstrings, clarifying its purpose and the expected behavior of its methods. These updates improve the code documentation and aid future developers in understanding the design pattern and implementation specifics.

--- src/spindle/interfaces/visitor_interface.py
+++ src/spindle/interfaces/visitor_interface.py
@@ -5,6 +5,24 @@ __All__ = ['IVisitor']
 
 
 class IVisitor(ABC):
+    """
+    Abstract base class representing a visitor in the Visitor design pattern.
+
+    This class defines the interface for concrete visitor classes.
+    """
+
     @abstractmethod
     def visit(self, element: Any):
+        """
+        Abstract method to be implemented by concrete visitor classes.
+
+        Args:
+            element (Any): The element to be visited.
+
+        Returns:
+            None
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a concrete subclass.
+        """
         pass
