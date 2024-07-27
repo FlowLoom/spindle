2c3b02827d1d860ad980e6c69d0e2387b32dcc10
Joshua Magady <josh.magady@gmail.com>
2024-07-27 16:06:09 -0500

    docs(processors): enhance docstrings for clarity and detail
    This commit updates the docstrings in the FileProcessor class, providing more detailed explanations of methods and their behaviors, including argument descriptions, return values, and exceptions.

--- src/spindle/processors/file_processor.py
+++ src/spindle/processors/file_processor.py
@@ -6,16 +6,28 @@ __All__ = ["FileProcessor"]
 
 
 class FileProcessor(AbstractFileProcessor):
-    """Concrete implementation of AbstractFileProcessor."""
+    """
+    Concrete implementation of AbstractFileProcessor for processing files.
+
+    This class provides methods to read and process file contents, removing empty lines,
+    comments, and non-ASCII characters.
+    """
 
     def process(self, file_path: str) -> List[str]:
-        """Processes a single file and returns its content as a list of strings.
+        """
+        Process a single file and return its content as a list of strings.
+
+        This method reads the file, removes empty lines, comments, and non-ASCII characters.
+        It handles IOErrors and logs them using the logging module.
 
         Args:
             file_path (str): The path to the file to be processed.
 
         Returns:
-            List[str]: The processed content of the file.
+            List[str]: The processed content of the file as a list of strings.
+
+        Raises:
+            IOError: If there's an issue reading the file (caught and logged internally).
         """
         content = []
         try:
@@ -31,5 +43,16 @@ class FileProcessor(AbstractFileProcessor):
 
     @staticmethod
     def remove_non_ascii(text: str) -> str:
-        """Removes non-ASCII characters from a string using encoding."""
+        """
+        Remove non-ASCII characters from a string using encoding.
+
+        This method encodes the input string to ASCII, ignoring non-ASCII characters,
+        then decodes it back to a string, effectively removing all non-ASCII characters.
+
+        Args:
+            text (str): The input string containing potential non-ASCII characters.
+
+        Returns:
+            str: The input string with all non-ASCII characters removed.
+        """
         return text.encode("ascii", errors="ignore").decode("ascii")
