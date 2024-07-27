1db662e2344c102b9ab21916d488bf62f421a8f2
Joshua Magady <josh.magady@gmail.com>
2024-07-27 01:00:09 -0500

    feat: Add IVisitable inheritance to IParser interface
    This change enhances the IParser interface to also be visitable, allowing it to support operations defined by the IVisitable interface. This is necessary for enabling new features that require visiting parsers as part of their process.

--- src/spindle/interfaces/parser_interface.py
+++ src/spindle/interfaces/parser_interface.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List
+from .visitable_interface import IVisitable
+
+__All__ = ['IParser']
 
 
-class IParser(ABC):
+class IParser(IVisitable):
     """Interface for parsers."""
 
     @abstractmethod
