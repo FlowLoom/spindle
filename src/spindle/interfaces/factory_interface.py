107d418aedda7f12e87a2b0488015b752cf3e4ac
Joshua Magady <josh.magady@gmail.com>
2024-07-27 15:47:07 -0500

    docs(interfaces): add docstrings to IParserFactory methods
    This commit enriches the `IParserFactory` class with detailed docstrings, providing descriptions for methods `create_parser`, `create_processor`, and `create_handler`, enhancing code readability and understanding.

--- src/spindle/interfaces/factory_interface.py
+++ src/spindle/interfaces/factory_interface.py
@@ -5,14 +5,46 @@ __All__ = ['IParserFactory']
 
 
 class IParserFactory(ABC):
+    """
+    Abstract base class defining the interface for a parser factory.
+
+    This factory is responsible for creating parsers, processors, and handlers.
+    """
+
     @abstractmethod
     def create_parser(self, *args, **kwargs) -> IParser:
+        """
+        Create and return a parser object.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            IParser: An instance of a class implementing the IParser interface.
+        """
         pass
 
     @abstractmethod
     def create_processor(self) -> IProcessor:
+        """
+        Create and return a processor object.
+
+        Returns:
+            IProcessor: An instance of a class implementing the IProcessor interface.
+        """
         pass
 
     @abstractmethod
     def create_handler(self, *args, **kwargs) -> IHandler:
+        """
+        Create and return a handler object.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            IHandler: An instance of a class implementing the IHandler interface.
+        """
         pass
