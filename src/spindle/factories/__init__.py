cddc86ef3243fa8caba649c2d499e944d7735d6a
Joshua Magady <josh.magady@gmail.com>
2024-07-27 14:20:00 -0500

    feat: Add versatile handler creation to CodeParserFactory
    Updated _create_concrete_handler to conditionally return ConsolePrintHandler or FileHandler based on provided arguments. This enhances the flexibility of handler instantiation by allowing different output configurations. Additionally, necessary imports were added to support the new logic.

---
+++ src/spindle/factories/__init__.py
@@ -0,0 +1,3 @@
+from .git_parser_factory import *
+from .code_parser_factory import *
+from .web_parser_factory import *
--- src/spindle/factories/code_parser_factory.py
+++ src/spindle/factories/code_parser_factory.py
@@ -1,7 +1,8 @@
 from spindle.abstracts.abstract_factory import AbstractParserFactory
 from spindle.parsers import CodeParser
 from spindle.processors import FileProcessor
-from spindle.handlers import FileHandler
+from spindle.handlers import FileHandler, ConsolePrintHandler
+from spindle.interfaces import IHandler
 
 __All__ = ['CodeParserFactory']
 
@@ -13,6 +14,16 @@ class CodeParserFactory(AbstractParserFactory):
     def _create_concrete_processor(self) -> FileProcessor:
         return FileProcessor()
 
-    def _create_concrete_handler(self, *args, **kwargs) -> FileHandler:
-        return FileHandler(*args, **kwargs)
+    def _create_concrete_handler(self, *args, **kwargs) -> IHandler:
+        # Check if 'console' or 'con' flag is present in kwargs
+        if kwargs.get('console') or kwargs.get('con'):
+            return ConsolePrintHandler()
+
+        # Check if 'output' is provided in kwargs
+        elif 'output' in kwargs:
+            return FileHandler(kwargs['output'])
+
+        # If no specific arguments are provided, default to ConsolePrintHandler
+        else:
+            return ConsolePrintHandler()
 
