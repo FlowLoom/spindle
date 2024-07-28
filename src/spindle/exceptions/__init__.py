5c83ac3a431560576a548da698d5709a1de682a9
Joshua Magady <josh.magady@gmail.com>
2024-07-28 14:39:23 -0500

    feat(exceptions, handlers): add handler_exception and composite_handler imports
    This commit adds new imports for handler_exception and composite

--- src/spindle/exceptions/__init__.py
+++ src/spindle/exceptions/__init__.py
@@ -1,2 +1,3 @@
 from .spindle_exceptions import *
 from .serialization_exceptions import *
+from .handler_exception import *
--- src/spindle/handlers/__init__.py
+++ src/spindle/handlers/__init__.py
@@ -1,2 +1,3 @@
 from .console_handler import *
 from .file_handler import *
+from .composite_handler import *
