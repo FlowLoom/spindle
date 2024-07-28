ab049d4ad70c493e952ea97ae21c843cd05949fb
Joshua Magady <josh.magady@gmail.com>
2024-07-27 20:35:20 -0500

    refactor(factories): enhance `CodeParserFactory` with configurable defaults
    This commit refactors `CodeParserFactory` to include configurable default settings for excluded directories, files, and file extensions, improving its flexibility and usability.

--- src/spindle/factories/code_parser_factory.py
+++ src/spindle/factories/code_parser_factory.py
@@ -1,8 +1,11 @@
-from spindle.abstracts.abstract_factory import AbstractParserFactory
+# In spindle/factories/code_parser_factory.py
+
+from spindle.abstracts import AbstractParserFactory
 from spindle.parsers import CodeParser
-from spindle.processors import FileProcessor
+from spindle.processors import CodeProcessor
 from spindle.handlers import FileHandler, ConsolePrintHandler
-from spindle.interfaces import IHandler
+from spindle.interfaces import IHandler, IProcessor
+from typing import List, Optional
 
 __All__ = ['CodeParserFactory']
 
@@ -11,53 +14,137 @@ class CodeParserFactory(AbstractParserFactory):
     """
     A factory class for creating code parsing components.
 
-    This class implements the Abstract Factory pattern to create concrete instances
-    of parsers, processors, and handlers for code parsing operations.
+    This class provides methods to create and configure CodeParser instances,
+    along with associated processors and handlers. It also manages default
+    settings for excluded directories, files, and file extensions.
     """
 
-    def _create_concrete_parser(self, *args, **kwargs) -> CodeParser:
+    def __init__(self):
         """
-        Create a concrete CodeParser instance.
+        Initialize the CodeParserFactory with default settings.
+        """
+
+        self.default_excluded_dirs = ['venv', '.git', '__pycache__']
+        self.default_excluded_files = ['setup.py', 'requirements.txt']
+        self.default_file_extensions = ['.py', '.js', '.html', '.css']
+
+    def _create_parser(self, *args, **kwargs) -> CodeParser:
+        """
+        Create and return a CodeParser instance.
 
         Args:
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
 
         Returns:
-            CodeParser: An instance of CodeParser with the created processor.
+            CodeParser: An instance of CodeParser configured with the specified
+                        or default settings.
         """
-        return CodeParser(self.create_processor(), *args, **kwargs)
 
-    def _create_concrete_processor(self) -> FileProcessor:
+        processor = self.create_processor(**kwargs)
+        excluded_dirs = kwargs.get('excluded_dirs', self.default_excluded_dirs)
+        excluded_files = kwargs.get('excluded_files', self.default_excluded_files)
+        file_extensions = kwargs.get('file_extensions', self.default_file_extensions)
+
+        return CodeParser(processor, excluded_dirs, excluded_files, file_extensions)
+
+    def _create_processor(self, **kwargs) -> IProcessor:
         """
-        Create a concrete FileProcessor instance.
+        Create and return a CodeProcessor instance.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments.
 
         Returns:
-            FileProcessor: An instance of FileProcessor.
+            IProcessor: An instance of CodeProcessor configured with the specified
+                        or default settings.
         """
-        return FileProcessor()
 
-    def _create_concrete_handler(self, *args, **kwargs) -> IHandler:
+        remove_comments = kwargs.get('remove_comments', True)
+        remove_empty_lines = kwargs.get('remove_empty_lines', True)
+        trim_lines = kwargs.get('trim_lines', True)
+
+        return CodeProcessor(remove_comments, remove_empty_lines, trim_lines)
+
+    def _create_handler(self, *args, **kwargs) -> IHandler:
         """
-        Create a concrete handler based on the provided arguments.
+        Create and return an appropriate handler instance.
 
         Args:
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
 
         Returns:
-            IHandler: An instance of a concrete handler (ConsolePrintHandler or FileHandler).
+            IHandler: An instance of a concrete handler (ConsolePrintHandler or FileHandler)
+                      based on the provided arguments.
         """
 
-        # Check if 'console' or 'con' flag is present in kwargs
         if kwargs.get('console') or kwargs.get('con'):
             return ConsolePrintHandler()
-
-        # Check if 'output' is provided in kwargs
-        elif 'output' in kwargs:
+        elif 'output' in kwargs and kwargs['output'] is not None:
             return FileHandler(kwargs['output'])
-
-        # If no specific arguments are provided, default to ConsolePrintHandler
         else:
             return ConsolePrintHandler()
 
+    def set_default_excluded_dirs(self, dirs: List[str]) -> None:
+        """
+        Set the default excluded directories.
+
+        Args:
+            dirs (List[str]): List of directory names to exclude.
+        """
+
+        self.default_excluded_dirs = dirs
+
+    def set_default_excluded_files(self, files: List[str]) -> None:
+        """
+        Set the default excluded files.
+
+        Args:
+            files (List[str]): List of file names to exclude.
+        """
+
+        self.default_excluded_files = files
+
+    def set_default_file_extensions(self, extensions: List[str]) -> None:
+        """
+        Set the default file extensions to include.
+
+        Args:
+            extensions (List[str]): List of file extensions to include.
+        """
+
+        self.default_file_extensions = extensions
+
+    def add_excluded_dir(self, dir: str) -> None:
+        """
+        Add a directory to the default excluded directories list.
+
+        Args:
+            dir (str): Directory name to exclude.
+        """
+
+        if dir not in self.default_excluded_dirs:
+            self.default_excluded_dirs.append(dir)
+
+    def add_excluded_file(self, file: str) -> None:
+        """
+        Add a file to the default excluded files list.
+
+        Args:
+            file (str): File name to exclude.
+        """
+
+        if file not in self.default_excluded_files:
+            self.default_excluded_files.append(file)
+
+    def add_file_extension(self, extension: str) -> None:
+        """
+        Add a file extension to the default file extensions list.
+
+        Args:
+            extension (str): File extension to include.
+        """
+
+        if extension not in self.default_file_extensions:
+            self.default_file_extensions.append(extension)
