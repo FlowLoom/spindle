a010c77e6e260ec97755dfef325702b72b01affe
Joshua Magady <josh.magady@gmail.com>
2024-07-27 22:31:46 -0500

    refactor: remove deprecated abstract and processor classes
    This commit removes deprecated classes (AbstractFileParser, AbstractFileProcessor, AbstractGitParser, DummyProcessor, FileProcessor) and their imports, cleaning up the codebase and preparing for future releases.

--- src/spindle/abstracts/__init__.py
+++ src/spindle/abstracts/__init__.py
@@ -1,9 +1,5 @@
 """Abstract classe"""
-from .abstract_file_parser import *
-from .abstract_file_processor import *
 from .abstract_handler import *
-from .abstract_git_parser import *
-from .abstract_web_parser import *
 from .abstract_cerebro import *
 from .abstract_factory import *
 from .abstract_visitor import *
--- src/spindle/processors/__init__.py
+++ src/spindle/processors/__init__.py
@@ -1,5 +1,3 @@
-from .file_processor import *
-from .dummy_processor import *
 from .web_processor import *
 from .code_processor import *
 from .git_commit_processor import *
