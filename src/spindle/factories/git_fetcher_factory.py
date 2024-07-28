3aa38c2050b0fb386d6b6082a63e352ad233b64e
Joshua Magady <josh.magady@gmail.com>
2024-07-28 14:39:01 -0500

    refactor(factory): enhance git fetcher factory with new features
    This commit refactors the GitFetcherFactory to include additional handler and serializer support, a timing decorator for fetchers, and updates to default settings, improving code flexibility and maintainability.

--- src/spindle/factories/git_fetcher_factory.py
+++ src/spindle/factories/git_fetcher_factory.py
@@ -1,8 +1,12 @@
-from spindle.abstracts import AbstractFetcherFactory
 from spindle.fetchers import GitCommitFetcher
 from spindle.processors import GitCommitProcessor
-from spindle.handlers import FileHandler, ConsoleHandler
-from spindle.interfaces import IHandler, IProcessor, IFetcher
+from spindle.handlers import FileHandler, ConsoleHandler, CompositeHandler, Color
+from spindle.serializers import ISerializer
+from .serializer_factory import SerializerFactory
+from spindle.decorators import TimingFetcherDecorator
+from spindle.abstracts import AbstractFetcherFactory
+from spindle.interfaces import IHandler, IFetcher
+from typing import List, Optional
 
 __All__ = ['GitFetcherFactory']
 
@@ -10,86 +14,50 @@ __All__ = ['GitFetcherFactory']
 class GitFetcherFactory(AbstractFetcherFactory):
     def __init__(self):
         self.default_extract_ticket_number = False
-        self.default_max_length = 72
+        self.default_max_length = -1
         self.default_capitalize_first_word = True
+        self.serializer_factory = SerializerFactory()
 
-    def _create_fetcher(self, *args, **kwargs) -> IFetcher:
-        """
-        Create and return a GitCommitParser instance.
-
-        Args:
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-
-        Returns:
-            GitCommitParser: An instance of GitCommitParser.
-        """
+    def _create_fetcher(self, stats: bool = False, **kwargs) -> IFetcher:
         processor = self._create_processor(**kwargs)
-        return GitCommitFetcher(processor)
+        fetcher = GitCommitFetcher(processor)
+        if stats:
+            fetcher = TimingFetcherDecorator(fetcher)
+        return fetcher
 
-    def _create_processor(self, **kwargs) -> IProcessor:
-        """
-        Create and return a GitCommitProcessor instance.
+    def _create_handler(self, output: Optional[str], format: str = 'plaintext', color: Optional[str] = None) -> IHandler:
+        serializer = self.serializer_factory.create_serializer(format)
+        handlers = []
 
-        Args:
-            **kwargs: Arbitrary keyword arguments.
+        if output:
+            file_handler = FileHandler(serializer, output)
+            handlers.append(file_handler)
 
-        Returns:
-            IProcessor: An instance of GitCommitProcessor.
-        """
-        extract_ticket_number = kwargs.get('extract_ticket_number', self.default_extract_ticket_number)
-        max_length = kwargs.get('max_length', self.default_max_length)
-        capitalize_first_word = kwargs.get('capitalize_first_word', self.default_capitalize_first_word)
+        console_handler = ConsoleHandler(serializer)
+        if color:
+            console_handler.set_color(Color[color.upper()])
+        handlers.append(console_handler)
 
+        if len(handlers) > 1:
+            composite_handler = CompositeHandler()
+            for handler in handlers:
+                composite_handler.add_handler(handler)
+            return composite_handler
+        else:
+            return handlers[0]
+
+    def _create_processor(self, **kwargs) -> GitCommitProcessor:
         return GitCommitProcessor(
-            extract_ticket_number=extract_ticket_number,
-            max_length=max_length,
-            capitalize_first_word=capitalize_first_word
+            extract_ticket_number=kwargs.get('extract_ticket_number', self.default_extract_ticket_number),
+            max_length=kwargs.get('max_length', self.default_max_length),
+            capitalize_first_word=kwargs.get('capitalize_first_word', self.default_capitalize_first_word)
         )
 
-    def _create_handler(self, *args, **kwargs) -> IHandler:
-        """
-        Create and return an appropriate handler instance.
-
-        Args:
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-
-        Returns:
-            IHandler: An instance of a concrete handler (ConsolePrintHandler or FileHandler)
-                      based on the provided arguments.
-        """
-
-        if kwargs.get('console') or kwargs.get('con'):
-            return ConsoleHandler()
-        elif 'output' in kwargs and kwargs['output'] is not None:
-            return FileHandler(kwargs['output'])
-        else:
-            return ConsoleHandler()
-
     def set_default_extract_ticket_number(self, extract: bool) -> None:
-        """
-        Set the default value for extracting ticket numbers.
-
-        Args:
-            extract (bool): Whether to extract ticket numbers by default.
-        """
         self.default_extract_ticket_number = extract
 
     def set_default_max_length(self, length: int) -> None:
-        """
-        Set the default maximum length for commit messages.
-
-        Args:
-            length (int): The default maximum length.
-        """
         self.default_max_length = length
 
     def set_default_capitalize_first_word(self, capitalize: bool) -> None:
-        """
-        Set the default value for capitalizing the first word.
-
-        Args:
-            capitalize (bool): Whether to capitalize the first word by default.
-        """
         self.default_capitalize_first_word = capitalize
